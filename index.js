5a8dfd37ccbe55d1f2b3b169b63ace2494f27427
HmSani6699 <shahinur04042002@gmail.com>
2023-05-15 21:51:55 +0600

    post

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express');
+const cors =require('cors');
 const app =express();
 const port =process.env.Port || 5000;
 
 
+// middleware
+app.use(cors());
+app.use(express.json());
 const users =[
-    {'id':1,'name':'john','email':'john@example.com'},
-    {'id':1,'name':'john','email':'john@example.com'},
-    {'id':1,'name':'john','email':'john@example.com'},
-    {'id':1,'name':'john','email':'john@example.com'}
+    {'id':1,'name':'john1','email':'john1@example.com'},
+    {'id':2,'name':'john2','email':'john2@example.com'},
+    {'id':3,'name':'john3','email':'john3@example.com'},
+    {'id':4,'name':'john4','email':'john4@example.com'}
 ]
 app.get('/users',(req,res)=>{
     res.send(users)
+})
+app.post('/users',(req,res)=>{
+    console.log('post api hitting ')
+    console.log(req.body)
+    const newUser=req.body;
+    newUser.id =users.length+1;
+    users.push(newUser);
+    res.send(newUser)
 })
 
 app.get('/',(req,res) =>{
---
+++ steps--.js
@@ -0,0 +1,9 @@
+/**
+ *create a post Api in server side 
+ * client side send data via post
+ *set fetch method inside the fetch options
+ *option will have three thing |method,headers,body
+ *headers:{'content-type':'application/son}
+   *send body :json.stringify (data)
+   *app.use(express.json()) middleware*
+   */ 
