18c62444a93903d0ff854cface7cee18cc7e5be6
HmSani6699 <shahinur04042002@gmail.com>
2023-05-15 16:57:42 +0600

    users

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const app =express();
 const port =process.env.Port || 5000;
 
 
-app.get('/',(req,res =>{
+const users =[
+    {'id':1,'name':'john','email':'john@example.com'},
+    {'id':1,'name':'john','email':'john@example.com'},
+    {'id':1,'name':'john','email':'john@example.com'},
+    {'id':1,'name':'john','email':'john@example.com'}
+]
+app.get('/users',(req,res)=>{
+    res.send(users)
+})
+
+app.get('/',(req,res) =>{
     res.send('user management system is running');
-}))
+})
 
 app.listen(port,()=>{
     console.log(`listen on port ${port}`)
